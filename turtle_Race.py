332ffcb66235ed2669b7d5a3dc97d167500cd57f
mohitgoel188 <35655238+mohitgoel188@users.noreply.github.com>
2018-01-29 00:49:26 +0530

    User Customization in turtle_Race

--- turtle_Race.py
+++ turtle_Race.py
@@ -2,45 +2,65 @@ from turtle import *
 import time
 import numpy as np
 
-def main():
-    speed(6)
-    for step in range(15):
+def race_course(speed_of_turtles=8,breadth=220,length=280):
+    speed(speed_of_turtles)
+    number_of_steps=int((length/20)+1)
+    for step in range(number_of_steps):
         write(step,align='center')
         penup()
         right(90)
         forward(10)
         pendown()
-        forward(220)
+        forward(breadth)
         penup()
-        backward(230)
+        backward(breadth+10)
         left(90)
         forward(20)
-    turt=np.empty(6,object)
-    pos=np.empty(6,object)
-    for i in range(6):
-        turt[i]=Turtle()
-        turt[i].speed(4)
-        turt[i].color([float(x) for x in np.random.rand(3)])
-        turt[i].shape('turtle')
-        turt[i].penup()
-        turt[i].goto(0,-20-40*i)
-        pos[i]=turt[i].position()
-        turt[i].pendown()
-    progress=np.zeros(6)    
-    #for turn in range(100):
+
+def turtles(breadth=220,number_of_turtles=6,speed_of_turtles=6):
+    turtle_mat=np.empty(number_of_turtles,object)
+    turtle_pos=np.empty(number_of_turtles,object)
+    for i in range(number_of_turtles):
+        turtle_mat[i]=Turtle()
+        turtle_mat[i].speed(speed_of_turtles)
+        turtle_mat[i].color([float(x) for x in np.random.rand(3)]) #color(.4,.3,.5)
+        turtle_mat[i].shape('turtle')
+        turtle_mat[i].penup()
+        turtle_mat[i].goto(0,-20-(breadth-20)/(number_of_turtles-1)*i)
+        turtle_pos[i]=turtle_mat[i].position()
+        turtle_mat[i].pendown()
+    return turtle_mat,turtle_pos
+
+def race(turtle_mat,turtle_pos,length=280,max_step=8):
+    progress=np.zeros(len(turtle_mat))    
     while True:
-        for i in range(6):
-            val=np.random.randint(1,5)
-            turt[i].forward(val)
+        for i in range(len(turtle_mat)):
+            val=np.random.randint(1,max_step)
+            turtle_mat[i].forward(val)
             progress[i]+=val
-            #print(i,'--',turt[i].position(),' VS ',pos[i])
-            if turt[i].position()<=pos[i]:
+            if turtle_mat[i].position()<=turtle_pos[i]:
                 time.sleep(3)
                 exit()
-            elif progress[i]>=280:
-                progress[i]=0
-                turt[i].right(180)
-        #print('')
+            elif progress[i]>=length:
+                progress[i]=-max_step
+                turtle_mat[i].right(180)
+def main():
+    print('\n\t\t\t\tTURTLE RACE\n\nEnter Details:-\t\t\t\t\t(Press only ENTER to pass default values)')
+    while True:
+        try:
+            l=int(input('Enter Length of Race Course: ') or '200')
+            b=int(input('Enter Breadth of Race Course: ') or '220')
+            s=int(input('Enter Speed of Turtles: ') or '8')
+            n=int(input('Enter Number of Turtles: ') or '6')
+            ms=int(input('Enter Max Step Size of Turtles: ') or '8')
+            break
+        except:
+            print('\nEnter Details correctly in INTEGERS.\n')
+            # race_course()
+            # race(*turtles())   
+    race_course(s,b,l)
+    turt,pos=turtles(b,n,s)
+    race(turt,pos,l,ms)
 
 if __name__ == '__main__':
     main()
